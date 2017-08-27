b87cb159a2b0a80780fec5c1729d80f579cb9844
Guillermo <gperez@libasoles.com>
2017-08-26 21:57:39 -0300

    Componente Contador

--- components/App/index.js
+++ components/App/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Encabezado from '../Encabezado'
+import Contador from '../Contador'
 
 const App = () => (
   <section>
-    <Encabezado titulo="React desde el principio"/>
+    <Encabezado titulo="React desde el principio" />
+    Meetup #<Contador numeroInicial={2} />
   </section>
 )
 
